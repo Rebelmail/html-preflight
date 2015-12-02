dd43ec84074c1cd09609ea18c62382b7727aca16
motdotla <mot@mot.la>
2015-12-01 16:13:12 -0800

    Integration test to catch bad html

--- test/preflight.test.js
+++ test/preflight.test.js
@@ -4,6 +4,15 @@ var expect = require('chai').expect;
 
 var HTMLPreflight = require('../lib/preflight');
 
+var plugins = require('../lib/plugins');
+var Prefixer = plugins.PrefixerPlugin;
+var Uglifier = plugins.UglifierPlugin;
+var Minifier = plugins.MinifierPlugin;
+
+var prefixer = new Prefixer();
+var uglifier = new Uglifier();
+var minifier = new Minifier();
+
 describe('HTMLPreflight', function() {
   describe('#constructor', function() {
     it('should not return null', function () {
@@ -45,6 +54,27 @@ describe('HTMLPreflight', function() {
       preflight.run('test', function(err, html) {
         expect(err).to.not.exist;
         expect(html).to.exist;
+        done();
+      });
+    });
+  });
+
+  describe('integration tests', function () {
+    var preflight;
+
+    before(function() {
+      preflight = new HTMLPreflight()
+    });
+
+    it('raises an error with invalid html', function(done) {
+      preflight.plugins = [
+        prefixer.run.bind(prefixer),
+        uglifier.run.bind(uglifier),
+        minifier.run.bind(minifier)
+      ]
+
+      preflight.run('<htm', function(err, html) {
+        expect(err).to.exist;
         done();
       });
     });
